78af6051942f68033b4ae3e8bf72378db923fe9d
omegakid1902 <anhdungbk09@gmail.com>
2021-09-13 00:14:48 +0700

    Update publish.py

--- publish.py
+++ publish.py
@@ -3,24 +3,29 @@ import frontmatter
 from shutil import copy
 import os
 
-if not os.path.exists('./docs'):
-    os.mkdir('./docs')
+if not os.path.exists('./content'):
+    os.mkdir('./content')
 
-def files_publish(root, file):
+for file in os.listdir("./zettelkasten/"):
     if file.endswith(".md"):
-        with open(os.path.join(root, file), encoding="utf8") as f:
+        with open(os.path.join("./zettelkasten/", file), encoding="utf8") as f:
             content = f.read()
             metadata, content = frontmatter.parse(content)
             if 'publish' in metadata.keys():
-                print("Copy publish files from zettelkasten to docs/")
-                print(os.path.join(root, file))
-                copy(os.path.join(root, file), './docs/')
+                print("Copy publish files from zettelkasten to content/")
+                copy(os.path.join("./zettelkasten/", file), './content/')
             else:
                 pass
 
-for file in os.listdir("./zettelkasten/"):
-    files_publish("./zettelkasten/", file)
-
 for root, dirs, files in os.walk("./zettelkasten/Zet/"):
-    files_publish("./zettelkasten/Zet/", file)
-
+    for file in files:
+        if file.endswith(".md"):
+            with open(os.path.join(root, file), encoding="utf8") as f:
+                content = f.read()
+                metadata, content = frontmatter.parse(content)
+                if 'publish' in metadata.keys():
+                    print("Copy publish files from zettelkasten to content/")
+                    print(os.path.join(root, file))
+                    copy(os.path.join(root, file), './content/')
+                else:
+                    pass
